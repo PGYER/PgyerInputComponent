234904fd2a0b34dc0aada38de6214a5158e561ed
lily <3034996598@qq.com>
2018-08-31 15:24:42 +0800

    Add demo

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,5 +4,36 @@ const app = getApp()
 
 Page({
   data: {
+    tel: "",
+    email: "",
+    verificationCode: "",
+    password: ""
+  },
+  isEmailOrTel: function(e) {
+    console.log("this is emial or tel", e.detail.pgyerResult);
+  },
+  isPhone: function(e) {
+    console.log("this is phone", e.detail.pgyerResult);
+  },
+  isVerificationCode: function(e) {
+    console.log("this is verification code", e.detail.pgyerResult);
+  },
+  isPassword: function(e) {
+    console.log("this is password", e.detail.pgyerResult);
+  },
+  confirm: function(e) {
+    console.log("click confirm button ", e);
+  },
+  pickCountryCode: function(e) {
+    console.log("click country code", e);
+  },
+  clickVerificationLabel: function(e) {
+    console.log("click verification label", e);
+  },
+  cancelCountDownAfterClick: function() {
+    let component = this.selectComponent('#component-id');
+    setTimeout(function(){
+      component.stopCuntDown();
+    }, 5000);
   }
 })
